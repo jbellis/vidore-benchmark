c3ebfd48b41eafc1e4c47c8093fbd7822dd1bbd2
Jonathan Ellis <jbellis@datastax.com>
2024-10-05 15:41:06 -0500

    fix pareto

--- plotone.py
+++ plotone.py
@@ -1,7 +1,8 @@
 import os
 import json
 import matplotlib.pyplot as plt
-import re
+import numpy as np
+from collections import defaultdict
 
 def parse_filename(filename):
     parts = filename.split('_')
@@ -24,20 +25,52 @@ def read_data(directory):
                 data_points.append((ndcg_at_5, qps, querypool, ann, candidates, dataset_name))
     return data_points
 
+def is_pareto_efficient(points):
+    is_efficient = np.ones(len(points), dtype=bool)
+    for i, point in enumerate(points):
+        if is_efficient[i]:
+            is_efficient[is_efficient] = np.any(points[is_efficient] > point, axis=1)
+            is_efficient[i] = True
+    return is_efficient
+
 def plot_data(data_points):
     fig, ax = plt.subplots(figsize=(12, 8))
     
-    ndcg_values, qps_values = zip(*[(d[0], d[1]) for d in data_points])
+    # Group data points by querypool
+    grouped_data = defaultdict(list)
+    for point in data_points:
+        grouped_data[point[2]].append(point)
     
-    scatter = ax.scatter(ndcg_values, qps_values)
+    # Generate a color map
+    color_map = plt.cm.viridis
+    colors = [color_map(i) for i in np.linspace(0, 1, len(grouped_data))]
+    
+    for (querypool, points), color in zip(grouped_data.items(), colors):
+        ndcg_values, qps_values = zip(*[(p[0], p[1]) for p in points])
+        
+        # Scatter plot for this querypool
+        scatter = ax.scatter(ndcg_values, qps_values, c=[color], label=f'querypool={querypool}')
+        
+        # Find Pareto optimal points
+        points_array = np.array(list(zip(ndcg_values, qps_values)))
+        pareto_mask = is_pareto_efficient(points_array)
+        pareto_points = points_array[pareto_mask]
+        
+        # Sort Pareto points by NDCG for line plot
+        pareto_points = pareto_points[pareto_points[:, 0].argsort()]
+        
+        # Plot Pareto frontier
+        ax.plot(pareto_points[:, 0], pareto_points[:, 1], c=color, linestyle='--')
+        
+        # Annotate points
+        for ndcg, qps, _, ann, candidates, _ in points:
+            label = f"{querypool},{ann},{candidates}"
+            ax.annotate(label, (ndcg, qps), xytext=(5, 5), textcoords='offset points', fontsize=8)
     
     ax.set_xlabel('NDCG@5')
     ax.set_ylabel('QPS')
     ax.set_title('NDCG@5 vs QPS')
-    
-    for i, (ndcg, qps, querypool, ann, candidates, dataset_name) in enumerate(data_points):
-        label = f"{querypool},{ann},{candidates}={ndcg:.3f}"
-        ax.annotate(label, (ndcg, qps), xytext=(5, 5), textcoords='offset points', fontsize=8)
+    ax.legend()
     
     plt.tight_layout()
     return fig

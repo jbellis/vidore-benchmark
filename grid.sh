5bf1f42302690f62cfc28dacab4409c9a1b2d75e
Jonathan Ellis <jbellis@datastax.com>
2024-10-06 08:05:31 -0500

    remove some of the less interesting points from grid.sh

--- grid.sh
+++ grid.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Arrays for each environment variable
-# VIDORE_DOC_POOL=(1 2 3 4 5)
-VIDORE_DOC_POOL=(2)
-VIDORE_QUERY_POOL=(0.0 0.01 0.015 0.02 0.03)
-VIDORE_N_ANN=(80 160 240 320)
+VIDORE_DOC_POOL=(1 2 3 4 5)
+# VIDORE_DOC_POOL=(2)
+VIDORE_QUERY_POOL=(0.01 0.02 0.03)
+VIDORE_N_ANN=(40 80)
 VIDORE_N_MAXSIM=(10 15 20 25)
 
 # Loop through all combinations
@@ -14,8 +14,8 @@ for doc_pool in "${VIDORE_DOC_POOL[@]}"; do
           for n_maxsim in "${VIDORE_N_MAXSIM[@]}"; do
               echo "VIDORE_DOC_POOL=$doc_pool, VIDORE_QUERY_POOL=$query_pool, VIDORE_N_ANN=$n_ann, VIDORE_N_MAXSIM=$n_maxsim"
               VIDORE_DOC_POOL=$doc_pool VIDORE_QUERY_POOL=$query_pool VIDORE_N_ANN=$n_ann VIDORE_N_MAXSIM=$n_maxsim \
-              vidore-benchmark evaluate-retriever --model-name colbert_live --dataset-name vidore/arxivqa_test_subsampled --split test
-#              vidore-benchmark evaluate-retriever --model-name colbert_live --collection-name "vidore/vidore-benchmark-667173f98e70a1c0fa4db00d" --split test
+              vidore-benchmark evaluate-retriever --model-name colbert_live --collection-name "vidore/vidore-benchmark-667173f98e70a1c0fa4db00d" --split test
+#              vidore-benchmark evaluate-retriever --model-name colbert_live --dataset-name vidore/arxivqa_test_subsampled --split test
               echo "----------------------------------------"
           done
       done
